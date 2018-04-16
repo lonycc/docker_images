1b91a9fdd7e22c0c047228860c2e5f1953f4d826
tony <xcc2533919@163.com>
2018-04-16 11:14:00 +0800

    Create hello.py

---
+++ nginx-gunicorn-flask/app/hello.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return jsonify({
+        'hello': 'world'
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
